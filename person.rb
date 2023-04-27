db1a0cfc3c2f8806f9e2fb5130376e5df3ac08a1
Mert Kantaroglu <mertkantaroglu@gmail.com>
2023-04-27 18:49:43 +0300

    Update file for choosing functionality

--- person.rb
+++ person.rb
@@ -8,7 +8,7 @@ class Person < Nameable
   attr_reader :id
   attr_accessor :name, :age, :parent_permission, :rentals
 
-  def initialize(age, name: 'Unknown', parent_permission: true)
+  def initialize(age:, parent_permission: true, name: 'Unknown')
     @id = Random.rand(1..1000)
     @age = age
     @name = name
@@ -39,12 +39,3 @@ class Person < Nameable
     Rental.new(date, book, self)
   end
 end
-
-person = Person.new(22, name: 'maximilianus')
-puts person.correct_name
-
-capitalized_person = CapitalizeDecorator.new(person)
-puts capitalized_person.correct_name
-
-capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
-puts capitalized_trimmed_person.correct_name
