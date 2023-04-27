1091cef97c2d590b55a2c0532cbe700fd5687c6d
Mert Kantaroglu <mertkantaroglu@gmail.com>
2023-04-27 18:49:55 +0300

    Update file for choosing functionality

--- student.rb
+++ student.rb
@@ -4,8 +4,8 @@ require_relative 'classroom'
 class Student < Person
   attr_accessor :classroom
 
-  def initialize(classroom, age, name: 'Unknown', parent_permission: true)
-    super(age, name, parent_permission)
+  def initialize(age, parent_permission, name)
+    super(age: age, name: name, parent_permission: parent_permission)
     @classroom = classroom
   end
 
